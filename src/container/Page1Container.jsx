18347bbbab9748266a3e5f13f9e038387e456b4b
ozvshhh <dlgkdms4660@gmail.com>
2024-01-10 20:10:08 +0900

    feat : 예시 파일 작성 예시파일은 코드 어떻게 쓰는지 참고하는 용도로 쓰면 됩니다.

---
+++ src/container/Page1Container.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Page1 from '../pages/page1/page1'
+
+export default function Page1Container() {
+  return (
+    <Page1/>
+  )
+}
---
+++ src/pages/page1/page1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './page1.scss'
+
+export default function Page1() {
+  return (
+    <div className='comPage1'>
+      <h>예시 페이지 입니다.</h>
+      <br/>
+      <button className='specialBtn'>버튼이오</button>
+    </div>
+  )
+}
