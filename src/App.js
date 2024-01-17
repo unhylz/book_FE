f7ab9ee1f50cb53eb3aff4d8403ebdf14d401fc0
jihyeon jeong <unhylz1110@naver.com>
2024-01-17 16:00:31 +0900

    feat : 글 작성 페이지 라우팅 추가

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import React from 'react';
 import {BrowserRouter,Routes, Route} from 'react-router-dom';
 
 //Container import는 여기 아래에 쭈르륵 해주세요
-import Page1Container from './container/Page1Container';
+//import Page1Container from './container/Page1Container';
+import SentimentWrite from './pages/page2/SentimentWrite';
+import SentimentDetail from './pages/page3/SentimentDetail';
 
 function App() {
   return (
     <>
       <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Page1Container></Page1Container>}/>
+        <Route path='/' element={<SentimentWrite></SentimentWrite>}/>
+        <Route path='/detail' element={<SentimentDetail></SentimentDetail>}/>
       </Routes>
       </BrowserRouter>
     </>
