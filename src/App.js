58ae1b555e22cfd8b3a49285d9c375035e2a7c94
ozvshhh <dlgkdms4660@gmail.com>
2024-01-17 15:50:44 +0900

    feat: passwordsearch, signup 라우팅 추가

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import {BrowserRouter,Routes, Route} from 'react-router-dom';
 //Container import는 여기 아래에 쭈르륵 해주세요
 import Page1Container from './container/Page1Container';
 import LoginContainer from './container/LoginContainer';
+import PasswordSearchContainer from './container/PasswordSearchContainer';
+import SignupContainer from './container/SignupContainer';
 
 function App() {
   return (
@@ -13,6 +15,10 @@ function App() {
       <Routes>
         <Route path='/' element={<Page1Container></Page1Container>}/>
         <Route path='/login' element={<LoginContainer/>}></Route>
+        <Route path='/passwordsearch' element={<PasswordSearchContainer/>}></Route>
+        <Route path='/signup' element={<SignupContainer/>}></Route>
+        
+        
       </Routes>
       </BrowserRouter>
     </>
