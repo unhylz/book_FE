6937b687cf8e4f3bea30a8fffc70101312ffaf89
dlskrud815 <dlskrud815@gmail.com>
2024-01-17 16:19:50 +0900

    fix: 충돌 해결
    Page1Container 삭제

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //Container import는 여기 아래에 쭈르륵 해주세요
 import HomeContainer from "./container/HomeContainer";
-import Login from "./components/login/Login";
 import SentimentLeagueContainer from "./container/SentimentLeagueContainer";
 import TopNavSearchContainer from "./container/TopNavSearchContainer";
-import Page1Container from "./container/Page1Container";
 import LoginContainer from "./container/LoginContainer";
 
 function App() {
