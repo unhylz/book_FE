2a4d3ce7579216bc951bf39eff28bc4030d1e23d
dlskrud815 <dlskrud815@gmail.com>
2024-01-17 15:52:03 +0900

    fix: 충돌 해결

--- src/App.js
+++ src/App.js
@@ -3,24 +3,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //Container import는 여기 아래에 쭈르륵 해주세요
-<<<<<<< Updated upstream
-import Page1Container from './container/Page1Container';
-
-=======
 import HomeContainer from "./container/HomeContainer";
 import Login from "./components/login/Login";
 import SentimentLeagueContainer from "./container/SentimentLeagueContainer";
 import TopNavSearchContainer from "./container/TopNavSearchContainer";
->>>>>>> Stashed changes
 function App() {
   return (
     <>
       <BrowserRouter>
-<<<<<<< Updated upstream
-      <Routes>
-        <Route path='/' element={<Page1Container></Page1Container>}/>
-      </Routes>
-=======
         <Routes>
           <Route path="/" element={<HomeContainer />} />
           <Route path="/login" element={<Login />}></Route>
@@ -33,7 +23,6 @@ function App() {
             element={<TopNavSearchContainer />}
           ></Route>
         </Routes>
->>>>>>> Stashed changes
       </BrowserRouter>
     </>
   );
