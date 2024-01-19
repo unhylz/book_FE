9ddc13f53513906b26847f349590353d5fb433d5
dlskrud815 <dlskrud815@gmail.com>
2024-01-19 09:45:47 +0900

    feat: 검색 후 닉네임 클릭 시 페이지 이동

---
+++ src/pages/NicknameDetail/NicknameDetail.jsx
@@ -0,0 +1,49 @@
+// NicknameDetail.jsx
+import React, { useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import Header from "../Home/components/header/Header";
+import SideAd from "../Home/components/advertisement/SideAd";
+import Footer from "../Home/components/footer/Footer";
+import "./NicknameDetail.scss";
+
+export default function NicknameDetail() {
+  // 선택한 센티먼트 id와 title 변수
+  const { content, nickname_title } = useParams();
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    navigate("/");
+  };
+
+  // 페이지 이동시 스크롤바 위치 최상단으로 가도록
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <div>
+      <Header onLogoClick={handleLogoClick} defaultSearchContent={content} />
+      <div className="main-content">
+        {/* 1열 - 왼쪽 사이드 광고 부분 */}
+        <div className="left">
+          <SideAd />
+        </div>
+
+        {/* 2열 - 중앙 메인 부분 */}
+        <div className="center">
+          <div className="contents">
+            <div>
+              <h2>NicknameDetail: {nickname_title}</h2>
+            </div>
+          </div>
+        </div>
+
+        {/* 3열 - 오른쪽 사이드 광고 부분 */}
+        <div className="right">
+          <SideAd />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
