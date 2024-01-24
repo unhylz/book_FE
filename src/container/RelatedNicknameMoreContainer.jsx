25db7063abb512ca3c2fa7962af91dbb066d90c8
dlskrud815 <dlskrud815@gmail.com>
2024-01-24 15:17:12 +0900

    feat: 검색 후 #닉네임

---
+++ src/container/RelatedNicknameMoreContainer.jsx
@@ -0,0 +1,7 @@
+// RelatedNicknameMoreContainer.jsx
+import React from "react";
+import RelatedNicknameMore from "../pages/RelatedNicknameMore/RelatedNicknameMore";
+
+export default function RelatedNicknameMoreContainer() {
+  return <RelatedNicknameMore></RelatedNicknameMore>;
+}
