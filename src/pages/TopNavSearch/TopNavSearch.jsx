5c7601024d3f5af93814941152798e5155d92b40
dlskrud815 <dlskrud815@gmail.com>
2024-01-24 15:25:18 +0900

    feat: 검색 페이지 관련 컴포넌트 연결

--- src/pages/TopNavSearch/TopNavSearch.jsx
+++ src/pages/TopNavSearch/TopNavSearch.jsx
@@ -1,23 +1,24 @@
 // TopNavSearch.jsx
 import React from "react";
 
-import { useNavigate, useLocation, Link } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import Header from "../Home/components/header/Header";
 import SideAd from "../Home/components/advertisement/SideAd";
 import Footer from "../Home/components/footer/Footer";
-import moreIcon from "../../assets/icons/moreicon.svg";
 import { bookDummy } from "./bookDummy";
-
+import { sentimentDummy } from "../Home/components/sentiment/sentimentDummy";
+import RelatedBook from "./components/RelatedBook/RelatedBook";
+import RelatedSentiment from "./components/RelatedSentiment/RelatedSentiment";
+import RelatedNickname from "./components/RelatedNickname/RelatedNickname";
 import "./TopNavSearch.scss";
 
 function TopNavSearch() {
   const navigate = useNavigate();
   const search_result = useLocation().state;
   const content = search_result ? search_result.content : "";
-
-  const displayedItems = bookDummy.slice(0, 3);
-
-
+  const displayedItems1 = bookDummy.slice(3, 6);
+  const displayedItems2 = sentimentDummy.slice(30, 33);
+  //const displayedItems3 = nicknameDummy.slice(0, 3);
   const handleLogoClick = () => {
     navigate("/");
   };
@@ -47,61 +48,26 @@ function TopNavSearch() {
             <div className="search-additional">
               <div className="related-book">
                 <h2>#관련서적</h2>
-                <div className="related-book-container">
-                  {displayedItems.map((result) => (
-                    <div key={result.id} className="search-result">
-                      <div className="related-book-info">
-                        <Link
-                          to={`/book/${search_result.content}/${result.book_title}`}
-                          className="more-book-link"
-                        >
-                          <img
-                            src={`bookcover_dummy/${result.image_file}`}
-                            alt={result.title}
-                            className="book-image"
-                          />
-                        </Link>
-                        <div className="none-img-detail-info">
-                          <Link
-                            to={`/book/${search_result.content}/${result.book_title}`}
-                            className="more-book-link"
-                          >
-                            <h3>{result.book_title}</h3>
-                          </Link>
-                          <div className="publish-info">
-                            <p>
-                              {result.publish_year}년 | {result.author} |
-                              {result.publisher}
-                            </p>
-                          </div>
-                          <div className="vote-info">
-                            <p>평균평점: </p>
-                            <p className="vote-avg">{result.vote_avg}</p>
-                            <p className="vote-num">({result.vote}명 참여)</p>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-                <div className="more-books">
-                  <Link
-                    to={`/${search_result.content}/related_book_more`}
-                    className="more-book-link"
-                  >
-                    <h3>더보기</h3>
-                    <img src={moreIcon} alt="moreIcon" className="more-icon" />
-                  </Link>
-                </div>
+                <RelatedBook
+                  searchResult={search_result}
+                  displayedItems={displayedItems1}
+                />
               </div>
               <div className="related-sentiment">
                 <h2>#센티먼트</h2>
+                <RelatedSentiment
+                  searchResult={search_result}
+                  displayedItems={displayedItems2}
+                />
               </div>
               <div className="related-nickname">
                 <h2>#닉네임</h2>
+                <RelatedNickname
+                  searchResult={search_result}
+                  displayedItems={displayedItems2} //---- 추후 3으로 수정
+                />
               </div>
             </div>
-
           </div>
         </div>
 
