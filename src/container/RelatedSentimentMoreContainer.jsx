28742b517f00594267fefe666299658fbffe9418
dlskrud815 <dlskrud815@gmail.com>
2024-01-24 15:17:28 +0900

    feat: 검색 후 #센티먼트

---
+++ src/container/RelatedSentimentMoreContainer.jsx
@@ -0,0 +1,7 @@
+// RelatedSentimentMoreContainer.jsx
+import React from "react";
+import RelatedSentimentMore from "../pages/RelatedSentimentMore/RelatedSentimentMore";
+
+export default function RelatedSentimentMoreContainer() {
+  return <RelatedSentimentMore></RelatedSentimentMore>;
+}
