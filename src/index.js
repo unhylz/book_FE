a3ce2baf23e2fa21693baf7c6efff703f173437d
dlskrud815 <dlskrud815@gmail.com>
2024-01-31 15:25:02 +0900

    feat: context Provider 추가

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { LoginProvider } from "./modules/api/login_context.js";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <LoginProvider>
+      <App />
+    </LoginProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-  
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
