723c41998a0cd6262f9fd9cbd6ef211a31836181
Gdw-ehddnr <ddd2678@naver.com>
2024-02-07 22:23:51 +0900

    feat: PasswordSearch 오류 수정

--- src/components/PasswordSearch/PasswordSearch.jsx
+++ src/components/PasswordSearch/PasswordSearch.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-import "../passwordSearch/passwordSearch.scss"
+import "../PasswordSearch/PasswordSearch.jsx"
 import BSL_logo from '../../assets/logos/BSL_logo.svg'
 import findPW_icon from '../../assets/icons/findPW.svg'
 import signup_icon from '../../assets/icons/signup.svg'
