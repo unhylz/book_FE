9349bbd8d7f924ff41b01ac5a87eef2c8c9d6f5d
dlskrud815 <dlskrud815@gmail.com>
2024-02-09 01:40:43 +0900

    feat: api 연결

--- src/pages/RelatedBookMore/components/RelatedBookResults.jsx
+++ src/pages/RelatedBookMore/components/RelatedBookResults.jsx
@@ -15,49 +15,45 @@ export default function RelatedBookResults({ searchResult, displayedItems }) {
     return `${year}`;
   }
 
+  //console.log("확인 데이터: ", displayedItems);
   return (
     <>
       <div className="related-book-results">
-        {displayedItems.bookObject &&
-          Array.isArray(displayedItems.bookObject) &&
-          displayedItems.bookObject.length > 0 &&
-          displayedItems.bookObject.map((result, index) => (
-            <div key={index} className="search-result">
-              <div className="related-book-info">
+        {displayedItems.map((result, index) => (
+          <div key={index} className="search-result">
+            <div className="related-book-info">
+              <Link
+                to={`/book/${searchResult}/${result.title}`}
+                className="more-book-link"
+              >
+                <img
+                  src={result.image}
+                  alt={result.title}
+                  className="book-image"
+                />
+              </Link>
+              <div className="none-img-detail-info">
                 <Link
                   to={`/book/${searchResult}/${result.title}`}
                   className="more-book-link"
                 >
-                  <img
-                    src={result.image}
-                    alt={result.title}
-                    className="book-image"
-                  />
+                  <h3>{result.title}</h3>
                 </Link>
-                <div className="none-img-detail-info">
-                  <Link
-                    to={`/book/${searchResult}/${result.title}`}
-                    className="more-book-link"
-                  >
-                    <h3>{result.title}</h3>
-                  </Link>
-                  <div className="publish-info">
-                    <p>
-                      {formatPublishYear(result.pubdate)}년 | {result.author} |{" "}
-                      {result.publisher}
-                    </p>
-                  </div>
-                  <div className="vote-info">
-                    <p>평균평점: &nbsp;</p>
-                    <p className="vote-avg">{result.avr_score}</p>
-                    <p className="vote-num">
-                      &nbsp; ({result.eval_num}명 참여)
-                    </p>
-                  </div>
+                <div className="publish-info">
+                  <p>
+                    {formatPublishYear(result.pubdate)}년 | {result.author} |{" "}
+                    {result.publisher}
+                  </p>
+                </div>
+                <div className="vote-info">
+                  <p>평균평점: &nbsp;</p>
+                  <p className="vote-avg">{result.avr_score}</p>
+                  <p className="vote-num">&nbsp; ({result.eval_num}명 참여)</p>
                 </div>
               </div>
             </div>
-          ))}
+          </div>
+        ))}
       </div>
     </>
   );
