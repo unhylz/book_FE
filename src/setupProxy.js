fa310fe4c30e1085747ea77adce42a06e4e595ea
dlskrud815 <dlskrud815@gmail.com>
2024-02-09 03:15:51 +0900

    feat: /ranks 추가

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,16 +2,8 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-  /*
   app.use(
-    createProxyMiddleware("/users", {
-      target: "http://3.37.54.220:3000",
-      changeOrigin: true,
-    })
-  );
-  */
-  app.use(
-    createProxyMiddleware(["/users", "/search"], {
+    createProxyMiddleware(["/users", "/search", "/ranks"], {
       target: "http://3.37.54.220:3000",
       changeOrigin: true,
     })
