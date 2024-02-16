1f370bd4412a5de6dbbd0cd00ae787cc247578b9
dlskrud815 <dlskrud815@gmail.com>
2024-02-16 21:54:36 +0900

    feat: 센티먼트 상세 페이지 댓글 데이터 연결 중

--- src/pages/SentimentDetail/Comment/Comment.jsx
+++ src/pages/SentimentDetail/Comment/Comment.jsx
@@ -1,5 +1,7 @@
-import './Comment.scss';
-import CommentDummy from './CommentDummy';
+import "./Comment.scss";
+import React, { useEffect, useState } from "react";
+
+import CommentDummy from "./CommentDummy";
 import userImg from "../../../assets/icons/user_Img.png";
 import RookieIcon from "../../../assets/tiers/루키.svg";
 import SilverIcon from "../../../assets/tiers/실버.svg";
@@ -7,14 +9,33 @@ import GoldIcon from "../../../assets/tiers/골드.svg";
 import DiaIcon from "../../../assets/tiers/다이아.svg";
 import MasterIcon from "../../../assets/tiers/마스터.svg";
 import GrandMasterIcon from "../../../assets/tiers/그랜드마스터.svg";
-import Notification from '../../page1/notification/notification';
+import Notification from "../../page1/notification/notification";
 import recommentIcon from "../../../assets/icons/comment.svg";
 import likeIcon from "../../../assets/icons/like.svg";
+import { SentimentIdSearch } from "../../../modules/api/search";
+export default function CommentItem(id) {
+  const [SearchData, setSearchData] = useState(null);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await SentimentIdSearch(id);
+        setSearchData(data);
+      } catch (error) {
+        console.error("데이터 가져오기 오류:", error);
+      }
+    };
 
-export default function CommentItem() {
+    fetchData();
+  }, [id]);
 
-	const getTierIcon = (tier) => {
+  useEffect(() => {
+    if (SearchData && SearchData[0].sentiment) {
+      console.log("센티먼트 데이터 확인용 33:", SearchData[0].sentiment);
+    }
+  }, [SearchData]);
+
+  const getTierIcon = (tier) => {
     const tierIcons = {
       루키: RookieIcon,
       실버: SilverIcon,
@@ -32,48 +53,62 @@ export default function CommentItem() {
     return SelectedIcon;
   };
 
-	return (
-		<div className='comment-list-item'>
-			{CommentDummy.map((result) => (
-				<div key={result.id} className="comment-result">
-					<div className='comment-header'>
-						<div className='list-top'>
-							<div className='profile-box'>
-								<img src={userImg} alt="Img" className="profile-image" style={{width: "36px", height: "36px"}}/>
-							</div>
-							<div className='info-box'>
-								<div className='name-tier'>
-									<div className='nickname'>{result.nickname}</div>
-									<img src={getTierIcon(result.tier)} alt="result.tier" className="tier-icon" />
-								</div>
-								<div className='time'>{result.writeDateTime}</div>
-							</div>
-						</div>
-						<div className='like-recomment-container'>
-							<div className="like">
-								<img src={likeIcon} alt="like" className="comment-like-icon" />
-							</div>
-							<div className='divider'></div>
-							<div className="recomment">
-								<img src={recommentIcon} alt="recomment" className="recomment-icon"/>
-							</div>
-						</div>
-					</div>
-					<div className='comment-main'>
-						<div className='content'>{result.content}</div>
-						<div className='comment-like-count'>
-							
-						</div>
-					</div>
-				</div>
-			))}
-			<div className="input-container">
-				<div className='text-box'>
-					<textarea className="textarea" placeholder="댓글을 작성하세요"></textarea>
-					<div className='text-count'>0/300</div>
-				</div>
-				<button className='comment-button'>{'작성하기'}</button>
-			</div>
-		</div>
-	)
+  return (
+    <div className="comment-list-item">
+      {CommentDummy.map((result) => (
+        <div key={result.id} className="comment-result">
+          <div className="comment-header">
+            <div className="list-top">
+              <div className="profile-box">
+                <img
+                  src={userImg}
+                  alt="Img"
+                  className="profile-image"
+                  style={{ width: "36px", height: "36px" }}
+                />
+              </div>
+              <div className="info-box">
+                <div className="name-tier">
+                  <div className="nickname">{result.nickname}</div>
+                  <img
+                    src={getTierIcon(result.tier)}
+                    alt="result.tier"
+                    className="tier-icon"
+                  />
+                </div>
+                <div className="time">{result.writeDateTime}</div>
+              </div>
+            </div>
+            <div className="like-recomment-container">
+              <div className="like">
+                <img src={likeIcon} alt="like" className="comment-like-icon" />
+              </div>
+              <div className="divider"></div>
+              <div className="recomment">
+                <img
+                  src={recommentIcon}
+                  alt="recomment"
+                  className="recomment-icon"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="comment-main">
+            <div className="content">{result.content}</div>
+            <div className="comment-like-count"></div>
+          </div>
+        </div>
+      ))}
+      <div className="input-container">
+        <div className="text-box">
+          <textarea
+            className="textarea"
+            placeholder="댓글을 작성하세요"
+          ></textarea>
+          <div className="text-count">0/300</div>
+        </div>
+        <button className="comment-button">{"작성하기"}</button>
+      </div>
+    </div>
+  );
 }
